38671d6a12db7ae55913a45f64310973494c7634
vooray <m.ukraintsev@gmail.com>
2020-09-21 21:52:51 +0300

    stats

--- packettik.go
+++ packettik.go
@@ -11,25 +11,39 @@ import (
 )
 
 func main() {
-	const tik_time = 1
+	const tikTime = 1
 	const timeout = 1
 	const hostname = "golang.org"
-	const port = 443
+	const port = 4433
+	const statsCycles = 10
 	connStr := hostname + ":" + strconv.Itoa(port)
-	//var success int = 0
+	var counterSuccess, counterFail, counterCycles int = 0, 0, 0
+	var chanSuccess chan bool = make(chan bool)
 
 	for {
-		go checkTcp(connStr, timeout)
-		time.Sleep(time.Duration(tik_time) * time.Second)
+		go checkTcp(connStr, timeout, chanSuccess)
+		counterCycles++
+		time.Sleep(time.Duration(tikTime) * time.Second)
+
+		if <-chanSuccess == true {
+			counterSuccess++
+		} else {
+			counterFail++
+		}
+		if counterCycles%statsCycles == 0 {
+			fmt.Println("---Stats: < Success: ", counterSuccess, "> / < Fail: ", counterFail, ">")
+		}
 	}
 }
 
-func checkTcp(connStr string, timeout int) {
+func checkTcp(connStr string, timeout int, chanSuccess chan bool) {
 	conn, err := net.DialTimeout("tcp", connStr, time.Duration(timeout)*time.Second)
 	if err != nil {
-		fmt.Println(time.Now().Format(time.ANSIC), "Fail! - Can not connect: ", err)
+		fmt.Println(time.Now().Format(time.ANSIC), "Fail! - Can not connect!", err)
+		chanSuccess <- false
 	} else {
 		fmt.Println(time.Now().Format(time.ANSIC), "OK! - Connected!")
 		conn.Close()
+		chanSuccess <- true
 	}
 }
